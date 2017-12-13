f11960159a0f162c3b87b18a5ffc7da0ed5ba89d
Thom Allen <thomwork@gmail.com>
2017-12-13 18:44:40 +0000

    Update DateInput tests: add interaction tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import App from './../App';
 import Life from './../Life';
 import Year from './../Year';
@@ -12,10 +12,15 @@ import ReactTestUtils from 'react-dom/test-utils';
 configure({ adapter: new Adapter() });
 
 describe('App', () => {
+  const app = shallow(<App />);
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+  it('should have initial state of birthDate: null', () => {
+    expect(app.state().birthDate).toBe(null);
+  });
+  it('should update its internal state when onBirthDateUpdate called');
 });
 
 describe('Life', () => {
@@ -54,13 +59,13 @@ describe('Label', () => {
   });
 })
 
-describe('DateInput', () => {
+describe('DateInput shallow tests', () => {
   const input = shallow(<DateInput />);
   it('renders a label', () => {
     expect(input.find('label').exists()).toEqual(true);
   });
   it('renders a numerical input', () => {
-    expect(input.find('input[type="submit"]').exists()).toEqual(true);
+    expect(input.find('input[type="date"]').exists()).toEqual(true);
   })
   it('renders a submit input', () => {
     expect(input.find('input[type="submit"]').exists()).toEqual(true);
@@ -68,9 +73,36 @@ describe('DateInput', () => {
   it('should have initial state of date: null', () => {
     expect(input.state().date).toBe(null);
   });
-  it('updates its internal state when form input submitted');
-  it('calls its onDateSubmit prop when state changes');
-  it('provides its state as an argument to onDateSubmit prop');
-  it('should throw if date isnt valid, eg submitted before input filled in');
-  it('should throw if date is in the future');
+});
+
+describe('DateInput interaction tests', () => {
+  const expectedDateValue = '01-01-1901';
+  let mockOnSubmit;
+  let mInput;
+  beforeEach(() => {
+    mockOnSubmit = jest.fn();
+    mInput = mount(<DateInput onSubmit={mockOnSubmit} />);
+    mInput.find('input[type="date"]').instance().value = expectedDateValue;
+  });
+  it('calls its onDateSubmit prop when state changes', () => {
+    mInput.find('form').simulate('submit', { preventDefault: jest.fn() });
+    expect(mockOnSubmit.mock.calls.length).toBe(1);
+  });
+  it('updates its internal state when form input submitted', () => {
+    mInput.find('form').simulate('submit', { preventDefault: jest.fn() });
+    expect(mInput.state().date).toBe(expectedDateValue);
+  });
+  it('provides input date value as an argument to onDateSubmit prop', () => {
+    mInput.find('form').simulate('submit', { preventDefault: jest.fn() });
+    expect(mockOnSubmit.mock.calls[0][0]).toBe(expectedDateValue);
+  })
+  it('should throw if date isnt valid, eg submitted before input filled in', () => {
+    /*
+      TODO: the DateInput method should probably throw an error which gets handled by an ErrorBoundry & we instead use .shouldThrowError() here
+    */
+    mInput.find('input[type="date"]').instance().value = 'WRONGVAL';
+    mInput.find('form').simulate('submit', { preventDefault: jest.fn() });
+    expect(mockOnSubmit.mock.calls.length).toBe(0);
+  });
+  it('should throw if date is in the future'); // TODO add date-fn mocking
 });
