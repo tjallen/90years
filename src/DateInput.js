7a7bd40a999e97b8cbf561b11ce64822d9497da8
Thom Allen <thomwork@gmail.com>
2017-12-13 17:20:40 +0000

    Update DateInput

--- src/DateInput.js
+++ src/DateInput.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class DateInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      date: null,
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    if (this._input.value.length !== 10) {
+      alert('please enter full birthdate');
+      return;
+    };
+    console.log('submitted', this._input.value, this._input.value.length)
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <label>Enter your birthday</label>
-        <input type="date" />
+        <input ref={(input) => this._input = input} type="date" />
+        <input type="submit" />
       </form>
     );
   }
